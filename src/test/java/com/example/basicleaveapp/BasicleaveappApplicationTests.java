1102cdbee1fe27b010c46ebae81e3c6b5083f453
Younus-Saberi <younussaberi@gmail.com>
2024-12-02 17:03:49 +0530

    initial commit

---
+++ src/test/java/com/example/basicleaveapp/BasicleaveappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basicleaveapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicleaveappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
